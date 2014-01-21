2056ecf999c5ae297c66253204400504c5e92b27
Kevin Nuut <kevin@krushcom.com>
2014-01-20 22:59:49 -0600

    Remove config

--- tests/Assembla.php
+++ tests/Assembla.php
@@ -20,17 +20,4 @@ class Example
 
         return $this->renderOutput('template_simple');
     }
-    
-    /**
-     * @return array
-     */
-    public function config()
-    {
-        return [
-            'apiUrl' => 'https://api.assembla.com/',
-            'apiKey' => 'eb13d041c3b1f6a605b4',
-            'apiSecret' => '44174ae401f092c01c41dd422f33610f46e13d19',
-            'spaceId' => 'krush'
-        ];
-    }
 }
